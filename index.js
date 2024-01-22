77f11c318bed5ec36d2f1d2854e5c63dc2a99eb6
erickgodinho <godinho.erickc@gmail.com>
2024-01-22 20:24:55 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-console.log ('asdadfgbhd')
-console.log('adfdhfghh')
+class heroi {
+    constructor(nome, idade, tipo){
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+    }
+    
+    atacar(){
+        let ataque = "";
+            switch(this.tipo){
+                case "mago":
+                ataque = "usou magia";
+                break;
+
+                case "guerreiro":
+                ataque = "usou espada";
+                break
+
+                case "monge":
+                ataque = "usou artes marciais";
+                break
+
+                case "ninja":
+                ataque = "usou shuriken";
+                break
+
+                default:
+                    ataque = " ataque estranho";
+            }
+            console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const guerreiro = new heroi("Arthur", 28, "guerreiro");
+const ninja = new heroi("leonardo", 18, "ninja")
+
+guerreiro.atacar();
+ninja.atacar();
